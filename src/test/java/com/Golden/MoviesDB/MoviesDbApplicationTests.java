24f7f067ce68e741b504f9b44abec3a9a753cd52
Zack <ZackGolden@pm.me>
2021-06-04 09:33:33 -0500

    Initial Commit

---
+++ src/test/java/com/Golden/MoviesDB/MoviesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Golden.MoviesDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
